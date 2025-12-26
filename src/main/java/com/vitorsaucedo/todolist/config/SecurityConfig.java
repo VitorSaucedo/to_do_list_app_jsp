d6641852fc0e294fbeaf9a4fed36950e4202ec5c
Vitor Saucedo <vitorsaucedo18@outlook.com>
2025-12-26 14:24:49 -0300

    fix: configure CSRF for JSP forms

--- src/main/java/com/vitorsaucedo/todolist/config/SecurityConfig.java
+++ src/main/java/com/vitorsaucedo/todolist/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +16,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
         http
                 .csrf(csrf -> {
                 })
